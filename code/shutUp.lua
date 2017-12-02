ce894aa283dc3d0045c47fc6e16514b1e95b9846
Saix_93 <r.torresgonzalez.93@gmail.com>
2017-12-02 18:20:05 +0100

    Añade una ventana de configuración para elegir qué módulos deben estar activos

--- code/shutUp.lua
+++ code/shutUp.lua
@@ -1,15 +1,17 @@
 -- Dice una frase cada vez que habla alguien
-local victimName = "Mellitharm-DunModr";
+local victimName = "Devilcloüd-DunModr";
 local frame = CreateFrame("FRAME");
 frame:RegisterEvent("CHAT_MSG_GUILD");
 
 local function shutUpEventHandler(self, event, ...)
+  if useShutUp then
     local msg, author = ...;
 
     if author == victimName then
-        local one, two = victimName:match("([^,]+)-([^,]+)");
-        SendChatMessage("Calla, " .. one, "GUILD");
+      local one, two = victimName:match("([^,]+)-([^,]+)");
+      SendChatMessage("Calla, " .. one, "GUILD");
     end
+  end
 end
 
 frame:SetScript("OnEvent", shutUpEventHandler);
